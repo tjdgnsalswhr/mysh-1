17e4cde090f6336bdfa5fa3499a0cfcfccd3129e
Lee SungHun <gldiwkd@ajou.ac.kr>
2017-11-08 13:38:26 +0900

    Process Creation complete and socketing

--- src/commands.c
+++ src/commands.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define SOCK_PATH "tpf_unix_sock.server"
+
 
 #include "commands.h"
 #include "built_in.h"
+
+
 
 static struct built_in_command built_in_commands[] = {
   { "cd", do_cd, validate_cd_argv },
@@ -30,29 +40,93 @@ static int is_built_in_command(const char* command_name)
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
-  if (n_commands > 0) {
+  if (n_commands == 1)
+  {
     struct single_command* com = (*commands);
 
     assert(com->argc != 0);
 
     int built_in_pos = is_built_in_command(com->argv[0]);
-    if (built_in_pos != -1) {
-      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
-        if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
+    if (built_in_pos != -1)
+    {
+      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv))
+      {
+        if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0)
+        {
           fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
         }
-      } else {
+      }
+      else
+      {
         fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
         return -1;
       }
-    } else if (strcmp(com->argv[0], "") == 0) {
-      return 0;
-    } else if (strcmp(com->argv[0], "exit") == 0) {
-      return 1;
-    } else {
-      fprintf(stderr, "%s: command not found\n", com->argv[0]);
-      return -1;
     }
+    else if (strcmp(com->argv[0], "") == 0)
+    {
+      return 0;
+    }
+    else if (strcmp(com->argv[0], "exit") == 0) 
+    {
+      return 1;
+    }
+    else
+    {
+      int pid;
+      pid = fork();
+      if(pid==0)
+      {
+         if(execv(com->argv[0], com->argv)==-1)
+         {
+           fprintf(stderr, "%s: command not found\n", com->argv[0]);
+           exit(0);
+         }
+      }
+    }
+  }
+
+  else if(n_commands>1)
+  {
+     int server_socket, client_socket;
+     int len, rc;
+     struct sockaddr_un server_sockaddr;
+     struct sockaddr_un client_sockaddr;
+     char buf[256];
+     memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+     memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+     memset(buf, 0, 256);
+     
+
+     server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+ 
+     if(server_socket == -1)
+     {
+       printf("SERVER PROGRAM SERVER SOCKET MAKINT FAIL\n");
+       exit(0);
+     }
+     else
+     {
+       printf("SERVER PROGRAM SERVER SOCKET MAKING SUCCESS\n");
+     }
+     
+     server_sockaddr.sun_family = AF_UNIX;
+     strcpy(server_sockaddr.sun_path, SOCK_PATH);
+     len = sizeof(server_sockaddr);
+     
+     unlink(SOCK_PATH);
+     rc = bind(server_socket, (struct sockaddr*)&server_sockaddr, len);
+     if(rc==-1)
+     {
+       printf("SERVER PROGRAM BIND ERROR\n");
+       close(server_socket);
+       exit(0);
+     }
+     else
+     {
+       printf("SERVER PROGRAM BIND SUCCESS\n");
+     }
+    
+
   }
 
   return 0;
